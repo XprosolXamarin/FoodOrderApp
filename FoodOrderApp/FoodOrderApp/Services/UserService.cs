f0af9e8bff704789be745b2e1c1d89c5a43a14e8
Luqman Akram <luqmanakram00300@outlook.com>
2020-12-11 05:27:43 -0800

    Add project files.

---
+++ FoodOrderApp/FoodOrderApp/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodOrderApp.Services
+{
+   public  class UserService
+    {
+        FirebaseClient client;
+        public UserService()
+        {
+            client = new FirebaseClient("https://foodorderapp-2b06f.firebaseio.com/");
+        }
+    }
+}
